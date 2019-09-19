2e85ba6c664f6216259a1f674327aa7b157959a3
Erik <erik.jonsson@arctoslabs.com>
2019-09-19 08:29:45 +0200

    comment

---
+++ build1.py
@@ -0,0 +1,11 @@
+print(" - Enter: Build 1")
+
+f = open("demo1.txt", "a")
+f.writelines(["1234567"])
+f.close()
+
+f = open("demo2.txt", "a")
+f.writelines(["1234567"])
+f.close()
+
+print(" - Exit:  Build 1")
