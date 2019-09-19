4ef2e9fc86b5380766d70df48797510ac98ec656
Erik <erik.jonsson@arctoslabs.com>
2019-09-19 08:52:46 +0200

    comment

--- test1.py
+++ test1.py
@@ -1,1 +1,17 @@
-print("Test 1")
+print(" - Enter: Test 1")
+
+try:
+    f = open("demo1.txt", "r")
+
+    if f.read() == "1234567":
+        print(" >>>> OK: demo1.txt")
+    else:
+        print(" <<<<<<<<< NOK")
+
+except:
+    print("Something went wrong when writing to the file")
+finally:
+    f.close()
+
+
+print(" - Exit:  Test 1")
