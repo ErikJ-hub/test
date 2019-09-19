f3e908d767bf940c3bc892fa93f744c0d29e4618
Erik <erik.jonsson@arctoslabs.com>
2019-09-19 11:22:47 +0200

    comment

--- test1.py
+++ test1.py
@@ -3,18 +3,18 @@ print(" - Enter: Test 1")
 try:
     f = open("demo1.txt", "r")
     a = f.read()
-    print(a)
-    if a == "11234567":
-        print(" >>>> OK: demo1.txt")
-    else:
-        print(" <<<<<<<<< NOK")
-        exit()
-
 except:
     print("Something went wrong when writing to the file")
-    exit()
 finally:
     f.close()
+
+print("     demo1.txt: " + a + " != 11234567")
+if a == "11234567":
+    print(" >>>> OK: demo1.txt")
+else:
+    print(" <<<<<<<<< NOK")
+    exit(1)
+
 
 
 print(" - Exit:  Test 1")
