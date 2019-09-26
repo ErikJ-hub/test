d83a49f2bb960ac7e4b0b769db11a50beb7d3355
Erik <erik.jonsson@arctoslabs.com>
2019-09-26 12:29:03 +0200

    comment

--- log.py
+++ log.py
@@ -4,9 +4,12 @@ import os
 test = os.getenv('TEST_LOGFILE')
 # test_logfile = os.getenv('USERNAME')
 if test is not None:
-    print(test)
+    logging.basicConfig(filename=test,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
 else:
-    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
 
 
 # for a in os.environ:
@@ -14,6 +17,6 @@ else:
 # print("all done")
 
 # level=logging.INFO  (CRITICAL, ERROR, WARNING, INFO or DEBUG) #
-logging.basicConfig(filename='test.log',
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+# logging.basicConfig(filename='test.log',
+#                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#                     level=logging.INFO)
